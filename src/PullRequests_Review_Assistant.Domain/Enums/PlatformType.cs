ba318623d93f5f5f5c36b2826f3ba86de72111ae
Thomas M Krystyan <217226122+thomas-mkrystyan_epam@users.noreply.github.com>
2026-03-21 17:47:16 +0100

    Add platform types

---
+++ src/PullRequests_Review_Assistant.Domain/Enums/PlatformType.cs
@@ -0,0 +1,12 @@
+namespace PullRequests_Review_Assistant.Domain.Enums
+{
+    /// <summary>
+    /// Supported source-control hosting platforms.
+    /// </summary>
+    public enum PlatformType
+    {
+        GitHub,
+        GitLab,
+        Bitbucket
+    }
+}
