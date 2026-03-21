0981c38b929edeffab7a719a172ac182c7bccb08
Thomas M Krystyan <217226122+thomas-mkrystyan_epam@users.noreply.github.com>
2026-03-21 16:44:45 +0100

    Init project

---
+++ PullRequests_Review_Assistant/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PullRequests_Review_Assistant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
